b4769485256b1e882f7a141aae14c8f023f1928e
Elias <localdomain>
2021-07-10 12:28:07 +0300

    Apply sensor callibration

--- src/decoder/parser.js
+++ src/decoder/parser.js
@@ -16,6 +16,13 @@ function parse(d) {
         return 0;
     }
     d.measurements.sort(compare);
+    if (d.offsetTemperature !== 0 || d.offsetHumidity !== 0 || d.offsetPressure !== 0) {
+        for (var i = 0; i < d.measurements.length; i++) {
+            if (d.offsetTemperature !== 0) d.measurements[i].parsed.temperature += d.offsetTemperature
+            if (d.offsetHumidity !== 0) d.measurements[i].parsed.humidity += d.offsetHumidity
+            if (d.offsetPressure !== 0) d.measurements[i].parsed.pressure += d.offsetPressure
+        }
+    }
     return d;
 }
 
