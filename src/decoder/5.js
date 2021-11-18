adac2b1663431d23d3a19b3e418eda6bc3f9bd16
Elias <localdomain>
2021-11-18 08:30:52 +0200

    #73 fixed invalid data checker

--- src/decoder/5.js
+++ src/decoder/5.js
@@ -10,28 +10,28 @@ const parseRawRuuvi = function (data) {
   robject.temperature = temperature / 200.0;
   robject.temperature = round(robject.temperature, 2);
 
-  if (data[5] !== 255 && data[6] !== 255) {
+  if (!(data[5] === 255 && data[6] === 255)) {
     robject.humidity = (((data[5] & 0xff) << 8) | (data[6] & 0xff)) / 400.0;
     robject.humidity = round(robject.humidity, 2);
   }
 
-  if (data[7] !== 255 && data[8] !== 255) {
+  if (!(data[7] === 255 && data[8] === 255)) {
     robject.pressure = (((data[7] & 0xff) << 8) | (data[8] & 0xff)) + 50000;
   }
 
-  if (data[9] !== 128 && data[10] !== 0) {
+  if (!(data[9] === 128 && data[10] === 0)) {
     let accelerationX = (data[9] << 8) | (data[10] & 0xff);
     if (accelerationX > 32767) accelerationX -= 65536; // two's complement
     robject.accelerationX = accelerationX;
   }
 
-  if (data[11] !== 128 && data[12] !== 0) {
+  if (!(data[11] === 128 && data[12] === 0)) {
     let accelerationY = (data[11] << 8) | (data[12] & 0xff);
     if (accelerationY > 32767) accelerationY -= 65536; // two's complement
     robject.accelerationY = accelerationY;
   }
 
-  if (data[13] !== 128 && data[14] !== 0) {
+  if (!(data[13] === 128 && data[14] === 0)) {
     let accelerationZ = (data[13] << 8) | (data[14] & 0xff);
     if (accelerationZ > 32767) accelerationZ -= 65536; // two's complement
     robject.accelerationZ = accelerationZ;
