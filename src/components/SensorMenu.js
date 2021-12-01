20aefcf74df2933db17bce17eebf1f1beee2b343
Elias <localdomain>
2021-12-02 00:57:03 +0200

    #75 stopped sensor menu from jumping around

--- src/components/SensorMenu.js
+++ src/components/SensorMenu.js
@@ -34,7 +34,7 @@ class SensorMenu extends Component {
     render() {
         const { t } = this.props;
         return (
-            <Menu autoSelect={false} strategy="fixed">
+            <Menu autoSelect={false} strategy="fixed" placement="bottom-end">
                 <MenuButton as={Button} rightIcon={<MdArrowDropDown size={20} color="#77cdc2" style={{ margin: -4 }} />} style={{ backgroundColor: "transparent", fontFamily: "mulish", fontSize: 16, fontWeight: "bold", paddingRight: 0 }}>
                     {t("sensors")}
                 </MenuButton>
