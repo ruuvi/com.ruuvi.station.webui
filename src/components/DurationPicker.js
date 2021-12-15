d43237f0ade4b31decccf65f7ac9aa9e6919ab18
Elias <localdomain>
2021-12-16 00:15:10 +0200

    #87 simplified history length ddl

--- src/components/DurationPicker.js
+++ src/components/DurationPicker.js
@@ -8,7 +8,7 @@ import {
 import { MdArrowDropDown } from "react-icons/md"
 import { useTranslation } from 'react-i18next';
 
-var timespans = [{ k: "1", t: "hour", v: 1 }, { k: "2", t: "hours", v: 2 }, { k: "8", t: "hours", v: 8 }, { k: "12", t: "hours", v: 12 }, { k: "1", t: "day", v: 24 }, { k: "2", t: "days", v: 24 * 2 }, { k: "3", t: "days", v: 24 * 3 }, { k: "1", t: "week", v: 24 * 7 }, { k: "2", t: "weeks", v: 24 * 7 * 2 }, { k: "1", t: "month", v: 24 * 7 * 4 }, { k: "2", t: "months", v: 24 * 7 * 4 * 2 }, { k: "3", t: "months", v: 24 * 7 * 4 * 3 }, { k: "6", t: "months", v: 24 * 7 * 4 * 6 }]
+var timespans = [{ k: "3", t: "hours", v: 3 }, { k: "1", t: "day", v: 24 }, { k: "3", t: "days", v: 24 * 3 }, { k: "1", t: "week", v: 24 * 7 }, { k: "1", t: "month", v: 24 * 7 * 4 }, { k: "3", t: "months", v: 24 * 7 * 4 * 3 }, { k: "1", t: "year", v: 24 * 7 * 4 * 12 }, { k: "2", t: "years", v: 24 * 7 * 4 * 12 * 2 }]
 
 export function getTimespans() { return timespans };
 
@@ -19,10 +19,12 @@ const detailedSubText = {
 
 export default function DurationPicker(props) {
     const { t } = useTranslation();
+    var ts = timespans.find(x => x.v === props.value)
+    if (!ts) ts = timespans[2];
     return (
         <Menu strategy="fixed" placement="bottom-end">
             <MenuButton as={Button} rightIcon={<MdArrowDropDown size={20} color="#77cdc2" style={{ margin: -4 }} />} style={{ backgroundColor: "transparent", ...detailedSubText }}>
-                {timespans.find(x => x.v === props.value).k} {t(timespans.find(x => x.v === props.value).t).toLowerCase()}
+                {ts.k} {t(ts.t).toLowerCase()}
             </MenuButton>
             <MenuList>
                 {timespans.map(x => {
