66476054002857291a3a5e5ef5e103199d8620ef
Elias <defragmated@gmail.com>
2022-02-07 00:40:35 +0200

    #110 signin inputs bg is now white

--- src/states/SignIn.js
+++ src/states/SignIn.js
@@ -126,7 +126,7 @@ class SignIn extends Component {
                                                 <Text style={infoText}>
                                                     {t("type_your_email")}
                                                 </Text>
-                                                <Input placeholder={t("email")} type="email" value={this.state.email} onChange={e => this.setState({ ...this.state, email: e.target.value })} autoFocus onKeyDown={this.emailKeyDown.bind(this)} />
+                                                <Input bg="white" placeholder={t("email")} type="email" value={this.state.email} onChange={e => this.setState({ ...this.state, email: e.target.value })} autoFocus onKeyDown={this.emailKeyDown.bind(this)} />
                                                 <Button colorScheme="teal" isDisabled={!this.emailIsValid()} onClick={this.register.bind(this)} style={buttonText}>{t("login")}</Button>
                                             </Stack>
                                         </SlideFade>
@@ -141,7 +141,7 @@ class SignIn extends Component {
                                                     <div style={{ textAlign: "center", width: "100%" }}>
                                                         <PinInput type="alphanumeric" value={this.state.validationCode} onChange={code => this.updateValidationCode(code)} autoFocus>
                                                             {Array(4).fill().map(() => {
-                                                                return <PinInputField style={{ margin: 5 }} />
+                                                                return <PinInputField bg="white" style={{ margin: 5 }} />
                                                             })}
                                                         </PinInput>
                                                     </div>
