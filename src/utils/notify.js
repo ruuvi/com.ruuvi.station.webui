5e4eb2bb6b28cdc1b726e72884b69dd51148c210
io53 <defragmated@gmail.com>
2022-03-15 22:59:15 +0200

    #143 tweaked toasts

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,4 +1,4 @@
-import { Alert, Text, createStandaloneToast } from '@chakra-ui/react'
+import { Alert, Text, createStandaloneToast, AlertIcon, CloseButton } from '@chakra-ui/react'
 import { ruuviTheme } from '../themes'
 
 function toastIt(title, status, duration) {
@@ -11,15 +11,24 @@ function toastIt(title, status, duration) {
                 alignItems="start"
                 borderRadius="md"
                 boxShadow="lg"
-                paddingEnd={8}
                 textAlign="start"
                 width="auto"
+                padding="20px"
+                status={status}
                 bg={ruuviTheme.colors.toast[status]}
                 color={status !== "info" ? "white" : undefined}
             >
+                <AlertIcon color={status !== "info" ? "white" : ruuviTheme.colors.gray} />
                 <Text fontWeight={status !== "info" ? 600 : undefined}>
                     {title}
                 </Text>
+                <CloseButton
+                    size="sm"
+                    onClick={props.onClose}
+                    position="absolute"
+                    insetEnd={1}
+                    top={1}
+                />
             </Alert>
         ),
     })
