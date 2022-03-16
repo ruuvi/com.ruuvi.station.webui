29ef27fa3e2da1903b7c9c5cb756eba2c124d2a6
io53 <defragmated@gmail.com>
2022-03-16 22:48:41 +0200

    #152 fixed units font in settings

--- src/components/RadioInput.js
+++ src/components/RadioInput.js
@@ -16,7 +16,7 @@ export default function RadioInput(props) {
             <FormLabel>{t(props.label)} {props.loading && <CircularProgress mt="-1" size="4" isIndeterminate={true} color="primary" />}</FormLabel>
             <RadioGroup value={props.value} onChange={v => props.onChange(v)}>
                 <Stack>
-                    {props.options.map(o => <Radio key={o.label} colorScheme="primaryScheme" value={o.value}>{t(o.label)}</Radio>)}
+                    {props.options.map(o => <Radio key={o.label} colorScheme="primaryScheme" value={o.value}>{t(o.label).replace("℃", "°C").replace("℉", "°F")}</Radio>)}
                 </Stack>
             </RadioGroup>
         </FormControl>
