8b2f7ef1bfdbccdb213fc12d4b9baa0b024a5182
io53 <defragmated@gmail.com>
2023-10-04 23:42:58 +0300

    #309 ordering for sensor menu

--- src/components/SensorMenu.js
+++ src/components/SensorMenu.js
@@ -12,6 +12,7 @@ import NetworkApi from "../NetworkApi";
 import withRouter from "../utils/withRouter"
 import { withTranslation } from 'react-i18next';
 import { useNavigate } from "react-router-dom";
+import { getSetting } from "../UnitHelper";
 
 
 class SensorMenu extends Component {
@@ -33,6 +34,16 @@ class SensorMenu extends Component {
         var path = this.props.router.location.pathname
         return path.substring(path.indexOf("/") + 1, path.length)
     }
+    getSensors() {
+        let order = getSetting("SENSOR_ORDER", null)
+        if (order) {
+            order = JSON.parse(order)
+            if (order) {
+                return order.map(x => this.state.sensors.find(y => y.sensor === x))
+            }
+        }
+        return this.state.sensors
+    }
     render() {
         const { t } = this.props;
         return (
@@ -44,13 +55,14 @@ class SensorMenu extends Component {
                     <MenuList mt="2" zIndex={10}>
                         <MenuItem className="ddlItem" style={{ borderTopLeftRadius: 6, borderTopRightRadius: 6, fontWeight: 800 }} onClick={() => this.props.addSensor()}>{t('add_new_sensor')}</MenuItem>
                         <MenuDivider />
-                        {this.state.sensors.map((x, i) => {
+                        {this.getSensors().map((x, i) => {
+                            if (!x) return null
                             let divider = <></>
                             let borderStyle = {};
                             if (i === this.state.sensors.length - 1) borderStyle = { borderBottomLeftRadius: 6, borderBottomRightRadius: 6 }
                             else divider = <MenuDivider />
                             return <div key={x.sensor + "div"}>
-                                <MenuItem key={x.sensor} className={(this.getCurrentSensor() === x.sensor ? "menuActive" : undefined)+ " ddlItem"} style={{ ...borderStyle }} onClick={() => this.props.navigate('/' + x.sensor)}>{x.name || x.sensor}</MenuItem>
+                                <MenuItem key={x.sensor} className={(this.getCurrentSensor() === x.sensor ? "menuActive" : undefined) + " ddlItem"} style={{ ...borderStyle }} onClick={() => this.props.navigate('/' + x.sensor)}>{x.name || x.sensor}</MenuItem>
                                 {divider}
                             </div>
                         })}
