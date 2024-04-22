8912994c0cd56da6a34a40dad2dd5899feff8583
io53 <defragmated@gmail.com>
2024-04-22 23:24:54 +0300

    #392 tweaked upgrade plan button style

--- src/components/UpgradePlanButton.js
+++ src/components/UpgradePlanButton.js
@@ -4,20 +4,24 @@ import i18next, { t } from 'i18next';
 
 const detailedSubText = {
     fontFamily: "mulish",
-    fontSize: "14px",
+    fontSize: "12px",
 }
 
 const UpgradePlanButton = () => {
     let url = "https://cloud.ruuvi.com";
     if (i18next.language === 'fi') url += '/fi';
+    let padding = 0.5;
     return (
         <a href={url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', ...detailedSubText }}>
             <Button size="small"
                 borderRadius={3}
                 paddingRight={2} paddingLeft={2} paddingBottom={1} paddingTop={0.5}
                 style={{ opacity: 0.75 }}
+                _hover={{ opacity: "1 !important" }}
                 onClick={e => e.preventDefault() || window.open(url, '_blank').focus()}>
-                {t('upgrade_plan')}
+                <div style={{ padding }}>
+                    {t('upgrade_plan')}
+                </div>
             </Button>
         </a>
     );
